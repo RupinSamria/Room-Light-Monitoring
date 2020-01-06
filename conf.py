28796a671ac5de104f58fcb4a29af78e5d1f2416
Rupin Samria <43100245+RupinSamria@users.noreply.github.com>
2020-01-06 17:04:07 +0530

    add Main and configuration file

---
+++ conf.py
@@ -0,0 +1,8 @@
+SSID = ''               #SSID in Twilio Dashboard 
+AUTH_TOKEN = ''         #Token from Twilio Dashboard 
+FROM_NUMBER = ''        #Generated nunber from Twilio
+TO_NUMBER = ''          #Mobile Number
+API_KEY = ''            #Bolt Cloud API
+DEVICE_ID = ''          #Bolt Device ID
+FRAME_SIZE = 10
+MUL_FACTOR = 6
